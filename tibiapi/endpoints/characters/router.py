5da4781480c354e92d8e5306db3bd3ef840ccbd4
Rafael Acioly <aciolyr@gmail.com>
2022-06-08 18:42:16 -0300

    adds empty endpoits for characters

--- tibiapi/endpoints/characters/router.py
+++ tibiapi/endpoints/characters/router.py
@@ -1,11 +1,43 @@
 from fastapi import APIRouter
 
-from tibiapi.endpoints.characters.schemas import Character
+from tibiapi.endpoints.characters.schemas import (Achievements, Badges,
+                                                  Character, Deaths, Guild,
+                                                  Information)
 
 router = APIRouter()
 
 
-@router.get("/{character_name}/", response_model=Character)
+@router.get("/{character_name}", response_model=Character)
 async def find(character_name: str) -> Character:
     """Find a character by his name."""
+    return {"character_name": character_name}
+
+
+@router.get("/{character_name}/achievements", response_model=Achievements)
+async def find_achievements(character_name: str) -> Achievements:
+    """Get achievements from a character."""
+    return {"character_name": character_name}
+
+
+@router.get("/{character_name}/badges", response_model=Badges)
+async def find_badges(character_name: str) -> Badges:
+    """Get badges from a character."""
+    return {"character_name": character_name}
+
+
+@router.get("/{character_name}/deaths", response_model=Deaths)
+async def find_deaths(character_name: str) -> Deaths:
+    """Get deaths from a character."""
+    return {"character_name": character_name}
+
+
+@router.get("/{character_name}/guild", response_model=Guild)
+async def find_guild(character_name: str) -> Guild:
+    """Get guild from a character."""
+    return {"character_name": character_name}
+
+
+@router.get("/{character_name}/information", response_model=Information)
+async def find_information(character_name: str) -> Information:
+    """Get information from a character."""
     return {"character_name": character_name}
--- tibiapi/endpoints/characters/schemas.py
+++ tibiapi/endpoints/characters/schemas.py
@@ -1,4 +1,70 @@
+from typing import List
+
 from pydantic import BaseModel
 
+
+class Badges(BaseModel):
+    name: str
+    icon_url: str
+    description: str
+
+
+class Information(BaseModel):
+    created: str
+    position: str
+    loyalty_title: str
+
+
+class Achievements(BaseModel):
+    grade: int
+    name: str
+    secret: bool = False
+
+
+class Guild(BaseModel):
+    name: str
+    rank: str
+
+
+class DeathAssist(BaseModel):
+    name: str
+    player: str
+    summon: str
+    traded: bool
+
+
+class DeathKiller(BaseModel):
+    name: str
+    player: str
+    summon: str
+    traded: bool
+
+
+class Deaths(BaseModel):
+    assists: List[DeathAssist]
+    killers: List[DeathKiller]
+
+
 class Character(BaseModel):
-    pass
+    name: str
+    sex: str
+    vocation: str
+    world: str
+    title: str
+    comment: str
+    deletion_date: str
+    account_status: str
+    former_names: List[str]
+    former_worlds: List[str]
+    achievement_points: int
+    last_login: str
+    level: int
+    married_to: str
+    residence: str
+    traded: bool
+    unlocked_titles: int
+    badges: List[Badges]
+    information: Information
+    guild: Guild
+    achievements: List[Achievements]
+    deaths: List[Deaths]
