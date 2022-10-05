89e5377c8ccea0d5e8d614cdcb656976dd462be6
Rafael Acioly <aciolyr@gmail.com>
2022-10-05 10:28:29 -0300

    docs: add docstring on api router builder

--- tibiapi/main.py
+++ tibiapi/main.py
@@ -4,11 +4,18 @@ from tibiapi.endpoints.characters.router import router as characters
 from tibiapi.endpoints.guilds.router import router as guilds
 from tibiapi.endpoints.worlds.router import router as worlds
 
-api_router = APIRouter()
+app = FastAPI(title="TibiAPI", version="0.0.1")
+api_router = APIRouter(prefix="/api/v1")
+
+"""
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where we can register API routes for the application.
+"""
 api_router.include_router(characters, prefix="/characters")
 api_router.include_router(guilds, prefix="/guilds")
 api_router.include_router(worlds, prefix="/worlds")
 
-
-app = FastAPI(title="TibiAPI", version="0.0.1")
 app.include_router(api_router)
