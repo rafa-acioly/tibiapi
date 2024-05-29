67f0b1d1cd19cc44d3597014724588dbca59e6e9
Rafael Acioly <aciolyr@gmail.com>
2024-05-29 11:53:08 -0300

    feat: organize routes file

--- tibiapi/main.py
+++ tibiapi/main.py
@@ -4,10 +4,13 @@ from fastapi_redis_cache import FastApiRedisCache
 from tibiapi.endpoints.characters.router import router as characters
 from tibiapi.endpoints.guilds.router import router as guilds
 from tibiapi.endpoints.worlds.router import router as worlds
+from tibiapi.routes import api_routes
 from tibiapi.tools.settings import Settings
 
 settings = Settings()
+
 app = FastAPI(title="TibiAPI", version="0.0.1")
+app.include_router(api_routes)
 
 
 @app.on_event("startup")
@@ -18,19 +21,3 @@ def startup():
         response_header="X-TibiApi-Cache",
         ignore_arg_types=[Request, Response]
     )
-
-
-api_router = APIRouter(prefix="/api/v1")
-
-"""
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where we can register API routes for the application.
-"""
-api_router.include_router(characters, prefix="/characters", tags=['Character'])
-api_router.include_router(guilds, prefix="/guilds", tags=['Guilds'])
-api_router.include_router(worlds, prefix="/worlds", tags=['Worlds'])
-
-app.include_router(api_router)
---
+++ tibiapi/routes.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+
+from tibiapi.endpoints.characters.router import router as characters
+from tibiapi.endpoints.guilds.router import router as guilds
+from tibiapi.endpoints.worlds.router import router as worlds
+
+api_routes = APIRouter(prefix="/api/v1")
+
+"""
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where we can register API routes for the application.
+"""
+api_routes.include_router(characters, prefix="/characters", tags=['Character'])
+api_routes.include_router(guilds, prefix="/guilds", tags=['Guilds'])
+api_routes.include_router(worlds, prefix="/worlds", tags=['Worlds'])
