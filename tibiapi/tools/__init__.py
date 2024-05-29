eca56215a66a8cdd63489229b830fe11b31d6172
Rafael Acioly <aciolyr@gmail.com>
2024-05-29 12:02:54 -0300

    fix: organize tool package

--- tibiapi/tools/__init__.py
+++ tibiapi/tools/__init__.py
@@ -1,36 +1,5 @@
 import re
 import unicodedata
-from collections import defaultdict
-
-
-def extract_table_information(content: list) -> dict:
-    """
-    Build a dictionary from the table content. This is mainly used when
-    the table has only two columns representing a key and a value
-    without any special formatting.
-
-    Example:
-        |      Account information      |
-        |---------------------|---------|
-        | Loyalty Title       | text    |
-        | Title               | text    |
-
-    The example above will generate a dict with the following fields:
-
-    Example:
-        {
-            "loyalty_title": "text",
-            "created": "text"
-        }
-    """
-
-    rows = zip(*(iter(content.find_all("td")),) * 2)
-    mapped_content = defaultdict(str)
-    for row in rows:
-        column_name, column_value = row
-        mapped_content[slugify(column_name.text)] = column_value.text
-
-    return mapped_content
 
 
 def slugify(value: str) -> str:
--- tibiapi/tools/strainer.py
+++ tibiapi/tools/strainer.py
@@ -1,10 +1,41 @@
+from collections import defaultdict
 from typing import Dict, List
 
 from bs4 import ResultSet
 
 from tibiapi.endpoints.characters.enums import CharacterPageIdentifiers
 from tibiapi.endpoints.characters.schemas import Achievements, Badges, Character
-from tibiapi.tools import extract_table_information
+from tibiapi.tools import slugify
+
+
+def extract_table_information(content: list) -> dict:
+    """
+    Build a dictionary from the table content. This is mainly used when
+    the table has only two columns representing a key and a value
+    without any special formatting.
+
+    Example:
+        |      Account information      |
+        |---------------------|---------|
+        | Loyalty Title       | text    |
+        | Title               | text    |
+
+    The example above will generate a dict with the following fields:
+
+    Example:
+        {
+            "loyalty_title": "text",
+            "created": "text"
+        }
+    """
+
+    rows = zip(*(iter(content.find_all("td")),) * 2)
+    mapped_content = defaultdict(str)
+    for row in rows:
+        column_name, column_value = row
+        mapped_content[slugify(column_name.text)] = column_value.text
+
+    return mapped_content
 
 
 def extract_basic_information(content: ResultSet) -> Dict[str, str]:
