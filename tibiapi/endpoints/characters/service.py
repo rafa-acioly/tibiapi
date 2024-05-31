2e885a293544a1010b52466e039be7c66d2bbdf9
Rafael Acioly <aciolyr@gmail.com>
2024-05-31 10:14:41 -0300

    doc: add doc on method that extract characters page

--- tibiapi/endpoints/characters/service.py
+++ tibiapi/endpoints/characters/service.py
@@ -13,9 +13,7 @@ async def get_character(character_name: str) -> Character:
     Get a character by his name.
     Scrape the Tibia.com website to get the character's information.
     """
-    page = await client.get_character(character_name)
-    page_tables = page.find_all(
-        "table", {"class": CharacterPageIdentifiers.TABLE_CONTENT.value})
+    page_tables = await _get_character_page_tables(character_name)
 
     return strainer.extract_basic_information(page_tables)
 
@@ -25,9 +23,7 @@ async def get_characters(character_name: str) -> List[Characters]:
     Get a list of all characters from a specific player.
     Scrape the Tibia.com website to get the character's information.
     """
-    page = await client.get_character(character_name)
-    page_tables = page.find_all(
-        "table", {"class": CharacterPageIdentifiers.TABLE_CONTENT.value})
+    page_tables = await _get_character_page_tables(character_name)
 
     return strainer.extract_characters(page_tables)
 
@@ -37,8 +33,19 @@ async def get_achievements(character_name: str) -> List[Achievements]:
     Get achievements from a character.
     Scrape the Tibia.com website to get the character's achievements.
     """
-    page = await client.get_character(character_name)
-    page_tables = page.find_all(
-        "table", {"class": CharacterPageIdentifiers.TABLE_CONTENT.value})
+    page_tables = await _get_character_page_tables(character_name)
 
     return strainer.extract_achievements(page_tables)
+
+
+async def _get_character_page_tables(character_name: str):
+    """
+    Get the character's page tables.
+    The page tables are the HTML tables that contain the character's information,
+    such as achievements, deaths, etc. All tables have the same class
+    name "TableContent".
+    """
+    page = await client.get_character(character_name)
+
+    return page.find_all(
+        "table", {"class": CharacterPageIdentifiers.TABLE_CONTENT.value})
